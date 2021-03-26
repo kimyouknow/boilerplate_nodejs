613a30fa87b3d93968fc251c688ae95a9ae7c082
yunho <kimyouknow@naver.com>
2021-03-27 08:44:24 +0900

    add loginPage with redux & hook

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,23 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+import {applyMiddleware, createStore} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from "./_reducers";
 
 const rootElement = document.getElementById("root");
+const createStoreWithMiddileware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 ReactDOM.render(
-  <StrictMode>
+  <Provider
+    store={createStoreWithMiddileware(Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && 
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+  >
     <App />
-  </StrictMode>,
+  </Provider>,
   rootElement
 );
